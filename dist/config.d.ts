8aa8bedbbb8c5e43a41ec1880f4b30494f989497
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-06-06 12:53:00 -0700

    chore: updating ncc build

--- dist/config.d.ts
+++
@@ -1,3 +0,0 @@
-import { type EnvironmentConfig } from './dtos';
-export declare const getEnvironmentConfig: () => EnvironmentConfig;
-export declare const setError: (errorMessage: string) => void;
--- dist/request.d.ts
+++
@@ -1,4 +0,0 @@
-import type { EnvironmentConfig, PackageInfo } from './dtos';
-export declare const getProjectPackages: (configuration: EnvironmentConfig) => Promise<PackageInfo[]>;
-export declare const deletePackage: (configuration: EnvironmentConfig, packageInfo: PackageInfo) => Promise<void>;
-export declare const deleteProject: (configuration: EnvironmentConfig) => Promise<void>;
